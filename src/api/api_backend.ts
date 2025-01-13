2773eee53c93c4dc39572f532c991787fddb2f77
mrubio7 <manuel.rubio.arias@gmail.com>
2025-01-13 13:07:10 +0100

    Refactor API backend host configuration: restore environment variable for backend host

--- src/api/api_backend.ts
+++ src/api/api_backend.ts
@@ -5,9 +5,9 @@ import { DTO_AuthFaceitCallback, DTO_UpdateUser } from "./dto/request"
 import { BuildPayload } from "@/libs/payload"
 
 const getHost = (): string => {
-    //let host = import.meta.env.VITE_BACKEND_HOST
-    return "http://localhost:8080/api/v2"
-    //return host
+    let host = import.meta.env.VITE_BACKEND_HOST
+    //return "http://localhost:8080/api/v2"
+    return host
 }
 
 export const ApiBackend = {
