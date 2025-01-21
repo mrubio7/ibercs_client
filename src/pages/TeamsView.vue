c0446bf6b53f3a36afb6d0752bf79bf97abdbbe9
mrubio7 <manuel.rubio.arias@gmail.com>
2025-01-21 21:11:01 +0100

    Add team management features in TeamsView; include AddTeamPopup and tooltip for user authentication

--- src/pages/TeamsView.vue
+++ src/pages/TeamsView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { ApiBackend } from '@/api/api_backend';
+import AddTeamPopup from '@/components/ibercs/team/components/AddTeamPopup.vue';
 import TeamLadder from '@/components/ibercs/team/components/TeamLadder.vue';
+import UserState from '@/components/ibercs/user/user_state';
+import Button from '@/components/ui/button/Button.vue';
 import Loader from '@/components/ui/loader/Loader.vue';
 import Title from '@/components/ui/title/Title.vue';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { Team } from '@/entities/team';
 import { onMounted, ref } from 'vue';
+
+document.title = "IBERCS - Equipos"
 
 const loading = ref<boolean>(true)
 const teams = ref<Team[]>([])
@@ -20,6 +26,17 @@ onMounted(async () => {
 <template>
     <div class="flex justify-between">
         <Title text="Equipos" />
+        <AddTeamPopup v-if="UserState.FaceitId != ''" />
+        <TooltipProvider v-else>
+            <Tooltip>
+                <TooltipTrigger>
+                    <Button size="sm" variant="secondary" :disabled="true">Añadir equipo</Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <span class="">Logueate para añadir un equipo</span>
+                </TooltipContent>
+            </ToolTip>
+        </TooltipProvider>
     </div>
     <section v-if="loading">
         <Loader />
